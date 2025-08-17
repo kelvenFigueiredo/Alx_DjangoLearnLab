a5b282cf286b7fdacddc61220ea7f827b62df4bf
KelvenFigueiredo <kelven.de.figueiredo.leopoldo@gmail.com>
2025-08-17 17:25:46 +0200

    FEAT: implementing validation in serialization file

--- advanced-api-project/advanced_api_project/settings.py
+++ advanced-api-project/advanced_api_project/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-9__%gd-ws_^j&ugqg#f8w^!w-b@2_)=5u--329%9*k4g7q9tbh'
+SECRET_KEY = 'django-insecure-==^gmeub8q%k7x(=pq#c*o!y(j-ylrn)!!zh&@m0)5-#d24&^('
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
